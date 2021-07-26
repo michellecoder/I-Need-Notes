19c8b24f2763c5d6f36950dd2b0531433bfb538a
Michelle Oakley <michellecoder38@gmail.com>
2021-07-26 13:04:12 -0700

    added info

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const fs = require('fs');
-
 const path = require('path');
+const express = require('express');
 
-const newNote = {
-    "title": "A New Note",
-    "text": "Testing text"
-};
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
-    const notes = JSON.parse(data);
+app.get('/', (req, res) => {
+            res.send('URL');
 
-    const notesJSON = JSON.stringify(notes);
-});
+            app.get('/notes', (req, res) => {
+                res.send('NOTES');
+
+            });
+
+            app.get('/api/notes', (req, res) => {
+                res.send('API NOTES');
+
+            });
+
+            app.post('/api/notes', (req, res) => {
+                res.send('API POST');
+
+            });
+
+            app.get('*', (req, res) => {
+                        res.send('CATCH ALL');
+
+
+
+
+                        app.listen(PORT, () => {
+                            console.log(`App listening on PORT http://localhost:${PORT}`);
+                        });
+
+
+
+                        // const newNote = {
+                        //     "title": "A New Note",
+                        //     "text": "Testing text"
+                        // };
+
+                        // fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+                        //     const notes = JSON.parse(data);
+
+                        //     const notesJSON = JSON.stringify(notes);
+                        // });
