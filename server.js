2734183273a576fe06535ea66c2126813fb00849
Michelle Oakley <michellecoder38@gmail.com>
2021-07-25 18:48:26 -0700

    added new note varible

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 
 const path = require('path');
 
+const newNote = {
+    "title": "A New Note",
+    "text": "Testing text"
+};
 
 fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
     const notes = JSON.parse(data);
