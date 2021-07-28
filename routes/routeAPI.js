7863f88d4f9b74076cc1027a4c25efba8c357851
Michelle Oakley <michellecoder38@gmail.com>
2021-07-28 13:09:11 -0700

    added router.get and router.post to modular file

---
+++ routes/routeAPI.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    })
+});
+
+router.post('/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        console.log(newNote);
+        notes.push(newNote);
+        const jsonNotes = JSON.stringify(notes);
+        fs.writeFile('./db/db.json', jsonNotes, (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    })
+});
+
+module.exports = router
